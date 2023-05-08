6aa8f4b58f635749a54c9031cfec68f8d56673f6
Jahir Raihan <39403552+jahirraihan22@users.noreply.github.com>
2023-05-08 11:47:58 +0600

    Update cluster-setup.sh

--- Kubernetes/ClusterSetup/cluster-setup.sh
+++ Kubernetes/ClusterSetup/cluster-setup.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
 
 echo "Setting up docker"
-sudo su
 apt update -y
 apt install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
